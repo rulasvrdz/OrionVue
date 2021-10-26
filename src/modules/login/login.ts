0c5f04a80ccb9ef7076dbcacd0a4c61a52bba682
rulasvrdzsw <rverduzco@slashweb.com.mx>
2021-10-26 03:55:32 -0500

    Auth

--- src/modules/login/login.ts
+++ src/modules/login/login.ts
@@ -1,5 +1,5 @@
 import {Options, Vue} from 'vue-class-component';
-import {loginByAuth, loginByGoogle, loginByFacebook} from '@/services/auth';
+// import {loginByAuth, loginByGoogle, loginByFacebook} from '@/services/auth';
 import Checkbox from '@/components/checkbox/checkbox.vue';
 import Input from '@/components/input/input.vue';
 import Button from '@/components/button/button.vue';
@@ -31,41 +31,41 @@ export default class Login extends Vue {
         (this.appElement as HTMLElement).classList.remove('login-page');
     }
 
-    public async loginByAuth(): Promise<void> {
-        try {
-            this.isAuthLoading = true;
-            const token = await loginByAuth(this.email, this.password);
-            this.$store.dispatch('auth/login', token);
-            this.toast.success('Login succeeded');
-            this.isAuthLoading = false;
-        } catch (error: any) {
-            console.log(error);
-            this.toast.error(error.message);
-            this.isAuthLoading = false;
-        }
-    }
+    // public async loginByAuth(): Promise<void> {
+    //     try {
+    //         this.isAuthLoading = true;
+    //         const token = await loginByAuth(this.email, this.password);
+    //         this.$store.dispatch('auth/login', token);
+    //         this.toast.success('Login succeeded');
+    //         this.isAuthLoading = false;
+    //     } catch (error: any) {
+    //         console.log(error);
+    //         this.toast.error(error.message);
+    //         this.isAuthLoading = false;
+    //     }
+    // }
 
-    public async loginByFacebook(): Promise<void> {
-        try {
-            this.isFacebookLoading = true;
-            const token = await loginByFacebook();
-            this.$store.dispatch('auth/login', token);
-            this.isFacebookLoading = false;
-        } catch (error: any) {
-            this.toast.error(error.message);
-            this.isFacebookLoading = false;
-        }
-    }
+    // public async loginByFacebook(): Promise<void> {
+    //     try {
+    //         this.isFacebookLoading = true;
+    //         const token = await loginByFacebook();
+    //         this.$store.dispatch('auth/login', token);
+    //         this.isFacebookLoading = false;
+    //     } catch (error: any) {
+    //         this.toast.error(error.message);
+    //         this.isFacebookLoading = false;
+    //     }
+    // }
 
-    public async loginByGoogle(): Promise<void> {
-        try {
-            this.isGoogleLoading = true;
-            const token = await loginByGoogle();
-            this.$store.dispatch('auth/login', token);
-            this.isGoogleLoading = false;
-        } catch (error: any) {
-            this.toast.error(error.message);
-            this.isGoogleLoading = false;
-        }
-    }
+    // public async loginByGoogle(): Promise<void> {
+    //     try {
+    //         this.isGoogleLoading = true;
+    //         const token = await loginByGoogle();
+    //         this.$store.dispatch('auth/login', token);
+    //         this.isGoogleLoading = false;
+    //     } catch (error: any) {
+    //         this.toast.error(error.message);
+    //         this.isGoogleLoading = false;
+    //     }
+    // }
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,10 @@
 import {createStore} from 'vuex';
-import authModule from './auth';
+// import authModule from './auth';
 import uiModule from './ui';
 
 export default createStore({
     modules: {
-        auth: authModule,
+        // auth: authModule,
         ui: uiModule
     }
 });
